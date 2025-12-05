43e045e8667ad08353dd4cdc577034e9f1ac8cac
benja2998 <benja2998@duck.com>
2025-12-05 20:26:35 -0300

    Change uname sysname to 'Linux' and hardcode version info
    Updated the uname implementation to set sysname to 'Linux' and hardcoded release and version fields. This change makes uname output mimic a Linux environment instead of reporting actual Windows version details.

--- win32/uname.c
+++ win32/uname.c
@@ -4,27 +4,25 @@
 
 int uname(struct utsname *name)
 {
-	const char *unk = "unknown";
-	OSVERSIONINFO os_info;
-	SYSTEM_INFO sys_info;
+    const char *unk = "unknown";
+    OSVERSIONINFO os_info;
+    SYSTEM_INFO sys_info;
 
-	strcpy(name->sysname, "Windows_NT");
+    strcpy(name->sysname, "Linux");
 
-	if ( gethostname(name->nodename, sizeof(name->nodename)) != 0 ) {
-		strcpy(name->nodename, unk);
-	}
+    if ( gethostname(name->nodename, sizeof(name->nodename)) != 0 ) {
+        strcpy(name->nodename, unk);
+    }
 
-	memset(&os_info, 0, sizeof(OSVERSIONINFO));
-	os_info.dwOSVersionInfoSize = sizeof(OSVERSIONINFO);
+    memset(&os_info, 0, sizeof(OSVERSIONINFO));
+    os_info.dwOSVersionInfoSize = sizeof(OSVERSIONINFO);
 
-	GetVersionEx(&os_info);
-	sprintf(name->release, "%u.%u", (unsigned int)os_info.dwMajorVersion,
-			(unsigned int)os_info.dwMinorVersion);
-	sprintf(name->version, "%u", (unsigned int)os_info.dwBuildNumber);
-
-	GetSystemInfo(&sys_info);
-	switch (sys_info.wProcessorArchitecture) {
-	case PROCESSOR_ARCHITECTURE_AMD64:
+    sprintf(name->release, "5.4.300");
+    sprintf(name->version, "#1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 "); 
+    
+    GetSystemInfo(&sys_info);
+    switch (sys_info.wProcessorArchitecture) {
+    case PROCESSOR_ARCHITECTURE_AMD64:
 		strcpy(name->machine, "x86_64");
 		break;
 	case PROCESSOR_ARCHITECTURE_INTEL:
